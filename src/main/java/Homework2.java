98735ec8306cb50d78487ed24b19baeb6c769afb
Elnur <eeliyev@yahoo.com>
2019-10-18 23:56:51 +0400

    --fourthFinish

--- src/main/java/Homework2.java
+++ src/main/java/Homework2.java
@@ -4,28 +4,36 @@ public class Homework2
 {
     public static void main(String[] args)
     {
-        int i, j, x, y, z, RandomX, RandomY, PlayerX, PlayerY, Array[][];
+        int i, j, x, y, z, RandomX, RandomY, PlayerX, PlayerY;
+        String Array[][];
 
-        Array= new int[5][5];
+        Array= new String[6][6];
         Random number= new Random();
         RandomX=number.nextInt(5);
         RandomY=number.nextInt(5);
-        System.out.println(RandomX);
-        System.out.println(RandomY);
-        for (x=0;x<5;x++)
+        RandomX=RandomX+1;
+        RandomY=RandomY+1;
+        //System.out.println(RandomX);
+        //System.out.println(RandomY);
+        for (x=1;x<6;x++)
         {
-            for(y=0;y<5;y++)
+            for(y=1;y<6;y++)
             {
-                if (x==RandomX && y==RandomY)
-                {
-                    Array[x][y]=1;
-                }
-                else
-                {
-                    Array[x][y] = 0;
-                }
+                Array[x][y]="-";
             }
         }
+        System.out.println("` 0 | 1 | 2 | 3 | 4 | 5 |` ");
+        System.out.printf("` 1 | %s | %s | %s | %s | %s |` ",Array[1][1],Array[1][2],Array[1][3],Array[1][4],Array[1][5]);
+        System.out.println();
+        System.out.printf("` 2 | %s | %s | %s | %s | %s |` ",Array[2][1],Array[2][2],Array[2][3],Array[2][4],Array[2][5]);
+        System.out.println();
+        System.out.printf("` 3 | %s | %s | %s | %s | %s |` ",Array[3][1],Array[3][2],Array[3][3],Array[3][4],Array[3][5]);
+        System.out.println();
+        System.out.printf("` 4 | %s | %s | %s | %s | %s |` ",Array[4][1],Array[4][2],Array[4][3],Array[4][4],Array[4][5]);
+        System.out.println();
+        System.out.printf("` 5 | %s | %s | %s | %s | %s |` ",Array[5][1],Array[5][2],Array[5][3],Array[5][4],Array[5][5]);
+        System.out.println();
+        /*
         for (x=0;x<5;x++)
         {
             for(y=0;y<5;y++)
@@ -34,7 +42,7 @@ public class Homework2
             }
             System.out.println();
         }
-
+        */
         Scanner MyObj= new Scanner(System.in);
         do
         {
@@ -58,8 +66,41 @@ public class Homework2
                     PlayerY = MyObj.nextInt();
                 }while (PlayerY<1 || PlayerY>5);
             }
+            if (PlayerX!=RandomX || PlayerY!=RandomY) {
+                Array[PlayerX][PlayerY] = "*";
 
+                System.out.println("` 0 | 1 | 2 | 3 | 4 | 5 |` ");
+                System.out.printf("` 1 | %s | %s | %s | %s | %s |` ", Array[1][1], Array[1][2], Array[1][3], Array[1][4], Array[1][5]);
+                System.out.println();
+                System.out.printf("` 2 | %s | %s | %s | %s | %s |` ", Array[2][1], Array[2][2], Array[2][3], Array[2][4], Array[2][5]);
+                System.out.println();
+                System.out.printf("` 3 | %s | %s | %s | %s | %s |` ", Array[3][1], Array[3][2], Array[3][3], Array[3][4], Array[3][5]);
+                System.out.println();
+                System.out.printf("` 4 | %s | %s | %s | %s | %s |` ", Array[4][1], Array[4][2], Array[4][3], Array[4][4], Array[4][5]);
+                System.out.println();
+                System.out.printf("` 5 | %s | %s | %s | %s | %s |` ", Array[5][1], Array[5][2], Array[5][3], Array[5][4], Array[5][5]);
+                System.out.println();
+            }
+            else
+            {
+                Array[PlayerX][PlayerY]="x";
+
+                System.out.println("` 0 | 1 | 2 | 3 | 4 | 5 |` ");
+                System.out.printf("` 1 | %s | %s | %s | %s | %s |` ",Array[1][1],Array[1][2],Array[1][3],Array[1][4],Array[1][5]);
+                System.out.println();
+                System.out.printf("` 2 | %s | %s | %s | %s | %s |` ",Array[2][1],Array[2][2],Array[2][3],Array[2][4],Array[2][5]);
+                System.out.println();
+                System.out.printf("` 3 | %s | %s | %s | %s | %s |` ",Array[3][1],Array[3][2],Array[3][3],Array[3][4],Array[3][5]);
+                System.out.println();
+                System.out.printf("` 4 | %s | %s | %s | %s | %s |` ",Array[4][1],Array[4][2],Array[4][3],Array[4][4],Array[4][5]);
+                System.out.println();
+                System.out.printf("` 5 | %s | %s | %s | %s | %s |` ",Array[5][1],Array[5][2],Array[5][3],Array[5][4],Array[5][5]);
+                System.out.println();
+
+            }
         }
         while (PlayerX!=RandomX || PlayerY!=RandomY);
+
+
     }
 }

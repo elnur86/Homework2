88e227655da36a60dfeafad18dc035eca8b3f718
Elnur <eeliyev@yahoo.com>
2019-10-18 23:19:13 +0400

    --thirdShooting

--- src/main/java/Homework2.java
+++ src/main/java/Homework2.java
@@ -4,14 +4,14 @@ public class Homework2
 {
     public static void main(String[] args)
     {
-        int i, j, x, y, z, RandomX, RandomY, Array[][];
+        int i, j, x, y, z, RandomX, RandomY, PlayerX, PlayerY, Array[][];
 
         Array= new int[5][5];
         Random number= new Random();
         RandomX=number.nextInt(5);
         RandomY=number.nextInt(5);
-        //System.out.println(RandomX);
-        //System.out.println(RandomY);
+        System.out.println(RandomX);
+        System.out.println(RandomY);
         for (x=0;x<5;x++)
         {
             for(y=0;y<5;y++)
@@ -34,5 +34,32 @@ public class Homework2
             }
             System.out.println();
         }
+
+        Scanner MyObj= new Scanner(System.in);
+        do
+        {
+            System.out.print("Please enter line for fire [1-5]:");
+            PlayerX=MyObj.nextInt();
+
+            if (PlayerX<1 || PlayerX>5) {
+                System.out.println("pirt");
+                do {
+                    System.out.print("Please enter correct line number [1-5]:");
+                    PlayerX = MyObj.nextInt();
+                }while (PlayerX<1 || PlayerX>5);
+            }
+
+            System.out.print("Please enter bar for fire [1-5]:");
+            PlayerY=MyObj.nextInt();
+
+            if (PlayerY<1 || PlayerY>5) {
+                do {
+                    System.out.print("Please enter correct bar number [1-5]:");
+                    PlayerY = MyObj.nextInt();
+                }while (PlayerY<1 || PlayerY>5);
+            }
+
+        }
+        while (PlayerX!=RandomX || PlayerY!=RandomY);
     }
 }
